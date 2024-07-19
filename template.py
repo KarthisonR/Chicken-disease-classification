aaafebc0c6340883083b6c6050b1cea05a533a70
Karthison R <karthisonr6543@gmail.com>
2024-07-20 03:49:28 +0530

    ingestion

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s')
 
-project_name= "cnnCloud" #cnn classifier
+project_name= "cnnClassifier" #cnn classifier
 
 list_of_files = [
     ".github/workflows/.gitkeep",
